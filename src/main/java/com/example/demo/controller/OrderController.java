0205fa8a4885aa49812e2b3d8a006af0f6a4aee9
harshbl <birsinghharsh@gmail.com>
2024-10-06 12:14:39 -0600

    unit price

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -6,13 +6,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OrderController {
     @RequestMapping("/submitOrder")
-    public ModelAndView submitOrder(@ModelAttribute("order") Order order){
-        double pricePerUnit = 979.00;
+    public ModelAndView submitOrder(@ModelAttribute("order") Order order, @RequestParam("price") double pricePerUnit){
+
         double totalPrice = pricePerUnit * order.getQuantity();
         ModelAndView mview = new ModelAndView();
         mview.setViewName("show-order");
